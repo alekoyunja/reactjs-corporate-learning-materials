18a137fbb34ae40362c591bf72fc1bc19d34dc6a
Alptuğ <carpediem.2323@gmail.com>
2024-01-01 15:32:07 +0000

    ok

---
+++ react-manually-installation/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js", 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html", 
+    }),
+  ],
+  devServer: {
+    port: 3030,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        resolve: {
+          extensions: [".js", ".jsx"]
+        },
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, // styles files
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+        loader: "url-loader",
+        options: { limit: false },
+      },
+    ],
+  },
+};
