9bd15c56ae1dcd676cecc3e3c5b958d821ba7750
Alptuğ <carpediem.2323@gmail.com>
2024-01-02 14:57:33 +0000

    ok

--- react-manually-installation/src/App.jsx
+++ react-manually-installation/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
+import styles from "./App.module.css";
 
 const App = () => {
-    return <h1>Hello React</h1>;
+    return <h1 className={styles.title}>Hello React</h1>;
 };
 
 export default App;
