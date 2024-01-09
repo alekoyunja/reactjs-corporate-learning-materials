3f74b506d154c68eb93fdd544df9c304ab45613d
Alptuğ <carpediem.2323@gmail.com>
2024-01-09 07:40:25 +0000

    ok

--- lectures/lecture-1/src/components/ClassButton/index.jsx
+++ lectures/lecture-1/src/components/ClassButton/index.jsx
@@ -3,12 +3,27 @@ import React, { Component } from 'react';
 class ClassButton extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  onClicked(e) {
+    console.log(this.state.count);
+
+    //state' in değiştyirmek için kulllaılan method setState
+    // state' i birer birer arttırdık
+    this.setState(() => {
+
+      return {
+        count: this.state.count + 1
+      };
+    });
   }
 
   render() {
     return (
-      <button type={this.props.type}>{this.props.title}</button>
-      
+      <button type={this.props.type} onClick={(e) => this.onClicked(e)}>{this.props.title}</button>
     );
   }
 }
--- lectures/lecture-1/src/components/FunctionButton/index.jsx
+++ lectures/lecture-1/src/components/FunctionButton/index.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-
+import React, {useState} from "react";
+1
 // arrow function
 const FunctionButton = (props) => {
     // object destructuring
     const { title, type } = props;
+    const [count, setCount] = useState(0);
+
+    console.log(count);
 
     return (
-        <button type={type}>{title}</button>
+        <button type={type} onClick={() => setCount(() => count+1)}>{title}</button>
     );
 };
 
