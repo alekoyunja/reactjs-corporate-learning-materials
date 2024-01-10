862da0e57d877097ec630cfa5c7c748ff77b2c9d
Alptuğ <carpediem.2323@gmail.com>
2024-01-10 08:15:26 +0000

    ok

--- lectures/kurumsal-yapi/src/pages/login/View/LoginForm.jsx
+++ lectures/kurumsal-yapi/src/pages/login/View/LoginForm.jsx
@@ -3,29 +3,33 @@ import React from "react";
 const LoginForm = () => {
 
     const onSubmit = (e) => {
-        console.log(e);
+        // default davranışını kes
+        // örn: eğer link olsaydı başka sayfaya gitmesini engellerdik
+        e.preventDefault();
+        const data = new FormData(e.currentTarget);
+        const username = data.get("username");
+        const password = data.get("password");
+        console.log(username, password);
     }
     return (
 
         <form id="login-form"
             onSubmit={onSubmit}>
-
             <input
                 type="text"
                 id="username"
+                name="username"
                 className="input"
                 placeholder="T.C. Kimlik Numarası" />
-
             <input
                 type="password"
                 id="password"
                 className="input"
+                name="password"
                 placeholder="Şifre Giriniz!" />
-
             <button className="button" type="submit">Giriş Yap</button>
             <p className="forgot"> Şifremi Unuttum!</p>
         </form>
-
     )
 };
 
