d967e6598ee570b5c63bddf37f387a5d5587ae34
Alptuğ <carpediem.2323@gmail.com>
2024-01-10 08:07:11 +0000

    ok

--- lectures/kurumsal-yapi/src/pages/login/View/Title.jsx
+++ lectures/kurumsal-yapi/src/pages/login/View/Title.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Title = ({title}) => <h2>{title}</h2>;
+
+//type checking
+Title.propTypes = {
+    title: PropTypes.string.isRequired
+};
+
+//type default values
+Title.defaultProps = {
+    title: "{title}"
+};
 
 export default Title;
