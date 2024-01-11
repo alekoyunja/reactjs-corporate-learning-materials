0a171a270ba40e5f39e997fd90d6735ef909a647
Alptuğ <carpediem.2323@gmail.com>
2024-01-11 18:52:26 +0000

    ok

--- lectures/kurumsal-yapi/src/pages/User/Views/UserList.tsx
+++ lectures/kurumsal-yapi/src/pages/User/Views/UserList.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { UserContext, User } from "../Data/UserProvider";
 
 type UserListProps = {
@@ -9,19 +9,38 @@ const UserList = ({ users }: UserListProps) => {
     const { removeUser } = useContext(UserContext);
 
     return (
-        <div>
-            <h1>UserList</h1>
-            <table>
-                <tbody>
-                    {users.map((user: User) => (
-                        <tr key={user.id}>
-                            <td>{user.firstName}</td>
-                            <td><button onClick={() => removeUser(user.id)} type="button">Kullanıcı Sil</button></td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <section className="container">
+            <div className="row">
+                <div className="col-12 p-4">
+                    <div className="row">
+                        <div className="col-6">
+                            <h1>Kullanıcı Listesi</h1>
+                        </div>
+                        <div className="col-6 text-end">
+                            <button className="btn btn-success">Ekle</button>
+                        </div>
+                    </div>
+                    <table className="table">
+                        <tbody>
+                            {users.map((user: User) => (
+                                <tr key={user.id}>
+                                    <td>{user.firstName}</td>
+                                    <td className="text-end">
+                                        <button
+                                            className="btn btn-danger"
+                                            onClick={() => removeUser(user.id)}
+                                            type="button"
+                                        >
+                                            Kullanıcı Sil
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
     );
 };
 
