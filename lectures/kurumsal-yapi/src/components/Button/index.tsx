fca7fb3cb399d66532122f3f7555d63baa360e22
Alptuğ <carpediem.2323@gmail.com>
2024-01-12 13:42:02 +0000

    ok

--- lectures/kurumsal-yapi/src/components/Button/index.tsx
+++ lectures/kurumsal-yapi/src/components/Button/index.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import styles from "./Button.module.scss";
 
@@ -7,6 +8,13 @@ type ButtonProps = {
     className: string;
 };
 
+/**
+ * This function performs a specific operation.
+ * 
+ * @param {number} param1 - The first parameter.
+ * @param {string} param2 - The second parameter.
+ * @returns {boolean} - The result of the operation.
+ */
 const Button = ({ children, onClick, className }: ButtonProps) => (
     <button
         type="button"
--- lectures/kurumsal-yapi/src/pages/User/Views/UserRoutes.tsx
+++ lectures/kurumsal-yapi/src/pages/User/Views/UserRoutes.tsx
@@ -1,21 +1,17 @@
-import React, { useEffect, useContext } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 import EditUser from "./EditUser";
-import { UserContext } from "../Data/UserProvider";
 
 const UserRoutes: React.FC = () => {
-    const { state, getAllUsers } = useContext(UserContext);
-    const { users } = state;
-
-    useEffect(() => {
-        getAllUsers();
-    }, []);
-
+    const [response, loading, error] = useFetch("https://jsonplaceholder.typicode.com/users");
+    console.log(response, loading, error);
+    
     return (
         <Routes>
-            <Route index element={<UserList users={users} />} />
+            <Route index element={<UserList users={response} />} />
             <Route path="/kullanici-ekle" element={<CreateUser />} />
             <Route path="/:id" element={<EditUser />} />
             <Route path="*" element={<p>404</p>} />
